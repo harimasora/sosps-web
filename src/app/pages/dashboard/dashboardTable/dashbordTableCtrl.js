b791e224548d2d29438fa101acd89f91c4cbaa4b
Danilo Maia Rodrigues <zidalo@gmail.com>
2016-10-28 14:49:12 -0200

    Open hospital modal

--- src/app/pages/dashboard/dashboardTable/dashbordTableCtrl.js
+++ src/app/pages/dashboard/dashboardTable/dashbordTableCtrl.js
@@ -9,14 +9,29 @@
         .controller('DashboardTableCtrl', DashboardTableCtrl);
 
     /** @ngInject */
-    function DashboardTableCtrl($scope, $filter, editableOptions, editableThemes, $firebaseObject, $firebaseArray) {
+    function DashboardTableCtrl($scope, $firebaseArray, $uibModal) {
 
         var ref = firebase.database().ref().child('healthOperators');
-        var hospitals = $firebaseArray(ref);
-
-        $scope.hospitals = hospitals;
+        $scope.hospitals = $firebaseArray(ref);
 
         $scope.smartTablePageSize = 10;
+
+        $scope.open = function (page, size, hospital) {
+
+            $scope.hospital = hospital;
+
+            $uibModal.open({
+                animation: true,
+                templateUrl: page,
+                size: size,
+                scope: $scope,
+                resolve: {
+                    items: function () {
+                        return $scope.items;
+                    }
+                }
+            });
+        };
 
     }
 })();

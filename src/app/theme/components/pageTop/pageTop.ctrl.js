339d25884a27c0885133d4fdfaed09a6fdcadb56
Danilo Maia Rodrigues <zidalo@gmail.com>
2016-10-28 19:44:03 -0200

    Profile image

--- src/app/theme/components/pageTop/pageTop.ctrl.js
+++ src/app/theme/components/pageTop/pageTop.ctrl.js
@@ -5,7 +5,14 @@
         .controller('pageTopCtrl', pageTopCtrl);
 
     /** @ngInject */
-    function pageTopCtrl($scope, Auth, $state) {
+    function pageTopCtrl($scope, Auth, $state, $firebaseObject) {
+
+        var firebaseUser = Auth.$getAuth();
+
+        var ref = firebase.database().ref("users");
+        var profileRef = ref.child(firebaseUser.uid);
+        $scope.profile = $firebaseObject(profileRef);
+
 
         $scope.signOut = function() {
             Auth.$signOut().then(function () {

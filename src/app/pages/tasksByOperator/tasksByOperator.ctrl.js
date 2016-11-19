c4a221727baf39060e5da8b426ad1e9cfc5ff9e5
Danilo Maia Rodrigues <zidalo@gmail.com>
2016-11-19 21:30:27 -0200

    Cannot complete future tasks

--- src/app/pages/tasksByOperator/tasksByOperator.ctrl.js
+++ src/app/pages/tasksByOperator/tasksByOperator.ctrl.js
@@ -43,51 +43,78 @@
       var taskToAdd = $scope.task;
       taskToAdd.updateOn = firebase.database.ServerValue.TIMESTAMP;
 
-      $scope.tasks.$save(taskToAdd)
-        .then(function() {
-          // Update All Tasks List
+        if (validateDate(taskToAdd) == true) {
+            $scope.tasks.$save(taskToAdd)
+              .then(function() {
+                  // Update All Tasks List
 
-          allTasksRef.child(taskToAdd.$id).set({
-            operator: currentAuth.uid,
-            hospital: taskToAdd.hospital,
-            specialty: taskToAdd.specialty,
-            date: taskToAdd.date,
-            status: taskToAdd.status,
-            timeRange: taskToAdd.timeRange,
-            watingTime: taskToAdd.watingTime == null ? 0 : taskToAdd.watingTime,
-            updateOn: taskToAdd.updateOn
-          }).then(function() {
-              // Update hospital with time
-              if (taskToAdd.status == 'Concluído') {
-                var hospitalRef = hospitalsRef.child(taskToAdd.hospital);
-                var hospital = $firebaseObject(hospitalRef);
+                  allTasksRef.child(taskToAdd.$id).set({
+                      operator: currentAuth.uid,
+                      hospital: taskToAdd.hospital,
+                      specialty: taskToAdd.specialty,
+                      date: taskToAdd.date,
+                      status: taskToAdd.status,
+                      timeRange: taskToAdd.timeRange,
+                      watingTime: taskToAdd.watingTime == null ? 0 : taskToAdd.watingTime,
+                      updateOn: taskToAdd.updateOn
+                  }).then(function() {
+                        // Update hospital with time
+                        if (taskToAdd.status == 'Concluído') {
+                            var hospitalRef = hospitalsRef.child(taskToAdd.hospital);
+                            var hospital = $firebaseObject(hospitalRef);
 
-                hospital.$loaded().then(function () {
-                  var watingTime = {};
-                  var updateOn = {};
-                  watingTime[taskToAdd.specialty] = taskToAdd.watingTime;
-                  updateOn[taskToAdd.specialty] = taskToAdd.updateOn;
-                  hospital.watingTime = watingTime;
-                  hospital.updateOn = updateOn;
-                  hospital.$save().then(function () {
-                    $scope.modalInstance.close('Create Button Clicked');
-                    toastr.success('Suas informações foram salvas com sucesso!');
-                  })
-                })
-              } else {
-                $scope.modalInstance.close('Create Button Clicked');
-                toastr.success('Suas informações foram salvas com sucesso!');
-              }
-            })
-            .catch(function() {
-              toastr.error("Suas informações não foram salvas.", 'Erro');
-            });
+                            hospital.$loaded().then(function () {
+                                var watingTime = {};
+                                var updateOn = {};
+                                watingTime[taskToAdd.specialty] = taskToAdd.watingTime;
+                                updateOn[taskToAdd.specialty] = taskToAdd.updateOn;
+                                hospital.watingTime = watingTime;
+                                hospital.updateOn = updateOn;
+                                hospital.$save().then(function () {
+                                    $scope.modalInstance.close('Create Button Clicked');
+                                    toastr.success('Suas informações foram salvas com sucesso!');
+                                })
+                            })
+                        } else {
+                            $scope.modalInstance.close('Create Button Clicked');
+                            toastr.success('Suas informações foram salvas com sucesso!');
+                        }
+                    })
+                    .catch(function() {
+                        toastr.error("Suas informações não foram salvas.", 'Erro');
+                    });
 
-        })
-        .catch(function() {
-          toastr.error("Suas informações não foram salvas.", 'Erro');
-        })
+              })
+              .catch(function() {
+                  toastr.error("Suas informações não foram salvas.", 'Erro');
+              })
+        }
     };
+
+      function validateDate(taskToAdd) {
+          var taskHour = parseInt(taskToAdd.timeRange.slice(0,2)); // Returns 06, 08, 10, ...
+          var serverDate = new Date();
+          var serverDateBeginning = new Date().setHours(0,0,0,0);
+          var taskDate = new Date(taskToAdd.date).setHours(0,0,0,0);
+          if (serverDateBeginning >= taskDate) {
+              if (serverDateBeginning == taskDate){
+                  // Task on same day
+                  if (serverDate.getHours() >= taskHour) {
+                      return true;
+                  } else {
+                      toastr.error("Não é possível fazer a tarefa de um horário posterior.", 'Erro');
+                      return false;
+                  }
+              } else {
+                  //Task on the past
+                  return true;
+              }
+          } else {
+              // Task on the future
+              toastr.error("Não é possível fazer a tarefa de um dia posterior.", 'Erro');
+              return false;
+          }
+      }
 
   }
 })();
